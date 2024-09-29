58f65d1684db68b51e82f5b4e82ac7892f2f7898
Bojana Bojic <bojanabojic30@gmail.com>
2024-09-29 22:28:24 +0200

    Set up tailwind.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-	return <div data-testid="test">Hello</div>;
+	return <div data-testid="test" className="text-4xl flex w-full h-[100vh] justify-center items-center">Hello</div>;
 }
 
 export default App;
